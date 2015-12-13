31de5be5479882a1d3971ca83171ac090fae0c6e
Ivan Simeyko <iv.simeyko@gmail.com>
2015-12-13 19:49:55 +0200

    solve task_3.sh

--- module_3/exs/task_3.sh
+++ module_3/exs/task_3.sh
@@ -1,11 +1,38 @@
 #!/bin/bash
 
-name=Ivan
-age=35
-while [ (-n $name) && (-n $age) ]
+echo "enter your name:"
+read name
+    if [[ $name == '' ]]
+    then
+        echo 'bye'
+        exit
+    fi
+echo "enter your age:"
+read age
+
+while [ "$name" != '' ] && [ $age != 0 ]
 do
+    if [[ $age -le 16 ]]
+    then
+        echo "$name, your group is child"
+    elif [[ $age -gt 16 ]] && [[ $age -le 25 ]]
+    then
+        echo "$name, your group is youth"
+    elif [[ $age -gt 25 ]]
+    then
+        echo "$name, your group is adult"
+    fi
+
     echo "enter your name:"
     read name
+    
+    if [[ $name == '' ]]
+    then
+        break
+    fi
     echo "enter your age:"
+    read age
 done
+
+echo "bye"
 
